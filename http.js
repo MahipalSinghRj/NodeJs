11ddfddc8e2d6d466da0095889dacd973466a5e6
Mahipal <mshekhawat1995@gmail.com>
2023-03-25 22:35:47 +0530

    Product get by id

--- http.js
+++ http.js
@@ -3,17 +3,28 @@ const fs = require("fs");
 const index = fs.readFileSync("index.html", "utf-8");
 const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
 
-//const product=data.products[0];
-const product =
-  data.products && data.products.length > 0 ? data.products[0] : null;
-
-
-
-  
+const product=data.products;
+// const product =
+//   data.products && data.products.length > 0 ? data.products[0] : null;
 
 const server = http.createServer((req, res) => {
   console.log("server started 1");
   console.log(req.url);
+
+  if(req.url.startsWith('/product')){
+    const id=req.url.split('/')[2];
+    const product1=data.products;
+    const product=product1.find(p=>p.id===(+id));
+    console.log(product)
+    res.setHeader("content-Type", "text/html");
+      const modifiedIndex = index
+        .replace("**rating**", product.rating)
+        .replace("**url**", product.thumbnail)
+        .replace("**title**", product.title)
+        .replace("**price**", product.price);
+      res.end(modifiedIndex);
+      return;
+  }
 
   switch (req.url) {
     case "/":
@@ -24,15 +35,15 @@ const server = http.createServer((req, res) => {
       res.setHeader("content-Type", "application/json");
       res.end(JSON.stringify(data));
       break;
-    case "/product":
-      res.setHeader("content-Type", "text/html");
-      const modifiedIndex = index
-      .replace("**rating**", product.rating)
-      .replace("**url**", product.thumbnail)
-      .replace("**title**", product.title)
-      .replace("**price**", product.price);
-      res.end(modifiedIndex);
-      break;
+    // case "/product":
+    //   res.setHeader("content-Type", "text/html");
+    //   const modifiedIndex = index
+    //     .replace("**rating**", product.rating)
+    //     .replace("**url**", product.thumbnail)
+    //     .replace("**title**", product.title)
+    //     .replace("**price**", product.price);
+    //   res.end(modifiedIndex);
+    //   break;
     default:
       res.writeHead(404, "Page Not Found");
       res.end();
