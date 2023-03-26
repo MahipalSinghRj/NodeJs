3ed83efa81dada0022fa5dc4f37e01aec06c252e
Mahipal <mshekhawat1995@gmail.com>
2023-03-26 12:21:04 +0530

    Added chages of get product from ID

--- http.js
+++ http.js
@@ -2,28 +2,27 @@ const http = require("http");
 const fs = require("fs");
 const index = fs.readFileSync("index.html", "utf-8");
 const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
-
-const product=data.products;
-// const product =
-//   data.products && data.products.length > 0 ? data.products[0] : null;
+const product1 = data.products;
 
 const server = http.createServer((req, res) => {
   console.log("server started 1");
-  console.log(req.url);
+  console.log(req.url, req.method);
 
-  if(req.url.startsWith('/product')){
-    const id=req.url.split('/')[2];
-    const product1=data.products;
-    const product=product1.find(p=>p.id===(+id));
-    console.log(product)
+  if (req.url.startsWith("/product")) {
+    const id = req.url.split('/')[2];
+    console.log("ID is:", id);
+    //const product1 = data.products;
+    //console.log("Products is:", product1);
+    const product = product1.find(p => p.id === (+id));
+    console.log("Product is:",product);
     res.setHeader("content-Type", "text/html");
-      const modifiedIndex = index
-        .replace("**rating**", product.rating)
-        .replace("**url**", product.thumbnail)
-        .replace("**title**", product.title)
-        .replace("**price**", product.price);
-      res.end(modifiedIndex);
-      return;
+    const modifiedIndex = index
+      .replace("**rating**", product.rating)
+      .replace("**url**", product.thumbnail)
+      .replace("**title**", product.title)
+      .replace("**price**", product.price);
+    res.end(modifiedIndex);
+    return;
   }
 
   switch (req.url) {
@@ -35,15 +34,6 @@ const server = http.createServer((req, res) => {
       res.setHeader("content-Type", "application/json");
       res.end(JSON.stringify(data));
       break;
-    // case "/product":
-    //   res.setHeader("content-Type", "text/html");
-    //   const modifiedIndex = index
-    //     .replace("**rating**", product.rating)
-    //     .replace("**url**", product.thumbnail)
-    //     .replace("**title**", product.title)
-    //     .replace("**price**", product.price);
-    //   res.end(modifiedIndex);
-    //   break;
     default:
       res.writeHead(404, "Page Not Found");
       res.end();
